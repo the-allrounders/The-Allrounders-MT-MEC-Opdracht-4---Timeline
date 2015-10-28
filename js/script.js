615c7ae8a09f2435a20ade87e99012ff24236f9b
ianwensink <ianwensink@gmail.com>
2015-10-28 16:10:33 +0100

    Proxy change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(initTimeline);
 
 function initTimeline() {
-    $.getJSON("http://cors.io/?u=https://spreadsheets.google.com/feeds/list/1PtY6JJcwyV1nSwwRfVi1VvinXXk-yq3VZpqSCRb4A8U/od6/public/values?alt=json", printItems);
+    $.getJSON("https://jsonp.afeld.me/?url=https://spreadsheets.google.com/feeds/list/1PtY6JJcwyV1nSwwRfVi1VvinXXk-yq3VZpqSCRb4A8U/od6/public/values?alt=json", printItems);
     $("#timeline").on("click", "ul .item", checkItem);
 }
 
